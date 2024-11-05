f96da9b14526f8f15424d53064ed19f34e4c4b65
John Funk <email@johnfunk.com>
2024-11-05 13:40:23 -0700

    force an update to main branch

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ def hello_world():
     """Example Hello World route."""
     # name = os.environ.get("NAME", "World")
     # return f"Hello {name}!"
-    return "Olá, mundo!"
+    # return "Olá, mundo!"
+    return "Hello, world!"
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
